88905eed7ad593fc6dc89e539a83b9791ceafc9d
Moilv1 <116920039+Moilv1@users.noreply.github.com>
2024-01-19 01:00:10 +0100

    start modale
    modale start with some difficulties

---
+++ FrontEnd/assets/modale.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const introduction = document.querySelector("#introduction");
+
+//Creation de la bannière et bouton édition//
+let editingBanner;
+let editingButton;
+
