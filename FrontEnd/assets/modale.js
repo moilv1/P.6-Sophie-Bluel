086d219dfa63d9a822147ed6740cd222b5fc18a4
Moilv1 <116920039+Moilv1@users.noreply.github.com>
2024-01-26 14:05:20 +0100

    project V1
    the project appears to be finished pending a bug or other problem

--- FrontEnd/assets/modale.js
+++ FrontEnd/assets/modale.js
@@ -2,9 +2,22 @@ const body = document.querySelector("body");
 const header = document.querySelector("header");
 const portfolioTITLE = document.querySelector("#portfolio h2");
 const modalBox = document.querySelector("dialog");
-const modalContent = document.querySelector("modal_content");
-const closemodal = document.querySelector(".modal .modaldelete .modal_closing_icon")
-console.log(closemodal);
+const modalContent = document.querySelector(".modal_content");
+const closetrig1 = document.querySelector(".modal .modaldelete .modal_closing_icon");
+const closetrig2 = document.querySelector(".modal .modaladding .icons .modal_closing_icon");
+const addButton = document.querySelector(".modal .modaldelete .add-photo");
+const modalDelete = document.getElementById("modaldelete");
+const modalAdding = document.getElementById("modaladding");
+const previous_icon = document.querySelector(".previous_icon");
+const Image_preview = document.querySelector(".image_preview");
+const boxImage_preview = document.getElementById("image");
+const Image_upload =document.getElementById("image_upload");
+const LabelUpload = document.querySelector(".uploadcontainer label");
+const fa_image = document.getElementById("fa-image");
+const text_format =document.getElementById("text_format");
+const titre = document.getElementById("Titleinput");
+const select = document.getElementById("category_input");
+const formModale = document.querySelector(".formModale");
 
  
 //Creation de la bannière et bouton édition//
@@ -41,18 +54,57 @@ function removeToken() {
 window.addEventListener("unload", removeToken);
 
 
+function openModal() {
+    addingProjets();
+    modalBox.showModal();
+}
 // Ouverture de la Modale
 editingButton.addEventListener("click", () => {
-    addingProjets()
-    modalBox.showModal();
-    
-})
+    openModal();
+    modalAdding.classList.replace("modaladding_active", "modaladding");
+    modalDelete.classList.replace("modaldelete_disabled", "modaldelete");
+});
 
-// Fermeture de la Modale 
-closemodal.addEventListener("click", () => {
+function ModalClose() {
+    modalContent.innerHTML = "";
     modalBox.close();
+};
+// Fermeture de la Modale 
+closetrig1.addEventListener("click", ModalClose );
+closetrig2.addEventListener("click", ModalClose );
+
+modalBox.addEventListener("click", (event) => {
+    if(event.target === modalBox) {
+        ModalClose()
+    }
 })
 
+// ouverture modal addingphoto
+function Gotomodaladding() {
+    modalAdding.classList.replace("modaladding", "modaladding_active");
+    modalDelete.classList.replace("modaldelete", "modaldelete_disabled");
+    ResFormAdding()
+}
+
+addButton.addEventListener("click", Gotomodaladding )
+
+// retour à la modalDelete
+function GoBackModalDelete() {
+    modalAdding.classList.replace("modaladding_active", "modaladding");
+    modalDelete.classList.replace("modaldelete_disabled", "modaldelete");
+    ResFormAdding()
+}
+previous_icon.addEventListener("click", GoBackModalDelete);
+
+// reset formulaire modaladding 
+function ResFormAdding() {
+    Image_preview.src = "";
+    boxImage_preview.style.display = "none"
+    LabelUpload.style.visibility = "visible";
+    fa_image.style.visibility = "visible";
+    text_format.style.visibility = "visible";
+
+}
 
 // ajout des projets dans la modal 
 // modalContent
@@ -65,7 +117,83 @@ function addingProjets() {
             throw new Error("echec lors de l'appel API.");
         }
     })
+    .then((data) => {
+        data.forEach((element) => {
+            const Card = document.createElement("figure");
+            const image = document.createElement("img");
+            const description = document.createElement("figcaption");  
+
+            Card.className = "Card";
+            image.src = element.imageUrl;
+            description.innerHTML = `<button <i class="fa-regular fa-trash-can"></i></button`;
+            description.setAttribute("id", "deleteBtn");
+
+            // Suprimer un projet ciblé
+
+            description.addEventListener('click', (e) => {
+                fetch(`http://localhost:5678/api/works/${element.id}`, {
+                  method: 'DELETE',
+                  headers: {
+                    Authorization: `Bearer ${localStorage.token}`,
+                  },
+                })
+                  .then(response => {
+                    if (response.ok) {
+                      Card.remove();
+                      alert("Projet supprimé avec succès !");
+                    }
+                });
+            });
+
+            modalContent.appendChild(Card);
+            Card.appendChild(image);
+            Card.appendChild(description)
+        })
+        
+    })
 }
 
 
+// Réalisation de la preview de l'image 
+Image_upload.addEventListener('change', function () {
+    const file = this.files[0];
+    if (file){
+        const reader = new FileReader();
+        boxImage_preview.style.display = "flex";
+
+        reader.addEventListener("load", function() {
+            LabelUpload.style.visibility = "hidden";
+            fa_image.style.visibility = "hidden";
+            text_format.style.visibility = "hidden";
+
+            Image_preview.setAttribute("src", this.result);
+        });
+        reader.readAsDataURL(file);
+    } 
+});
+
+// ajout projet dans l'api 
+formModale.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const formData = new FormData();
+    formData.append("image", Image_upload.files[0]);
+    formData.append("title", titre.value);
+    formData.append("category", select.value);
+  
+    fetch(APIpathWorks, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    }).then((reponse) => {
+      if (reponse.ok) {
+        alert("Projet ajouté avec succès !");
+        removeGalleryPreview()
+      } else {
+        alert("Le formulaire est incomplet!");
+      }
+    });
+});
 
